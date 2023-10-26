2d40b5c20e41e27be226fa02d733aa3766c62a37
pockocmoc <pockocmoc83@gmail.com>
2023-10-26 06:52:01 +0300

    second commit

--- src/main/java/com/pockocmoc/DiningPhilosophers.java
+++ src/main/java/com/pockocmoc/DiningPhilosophers.java
@@ -1,4 +1,28 @@
 package com.pockocmoc;
 
 public class DiningPhilosophers {
+    private static final int NUM_PHILOSOPHERS = 5;
+    static final int NUM_EATING_ITERATIONS = 3;
+
+    public static void main(String[] args) {
+        Philosopher[] philosophers = new Philosopher[NUM_PHILOSOPHERS];
+        Object[] forks = new Object[NUM_PHILOSOPHERS];
+
+
+        for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+            forks[i] = new Object();
+        }
+
+
+        for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+            Object leftFork = forks[i];
+            Object rightFork = forks[(i + 1) % NUM_PHILOSOPHERS];
+
+            philosophers[i] = new Philosopher(i, leftFork, rightFork);
+        }
+
+        for (Philosopher philosopher : philosophers) {
+            philosopher.start();
+        }
+    }
 }
--- src/main/java/com/pockocmoc/Philosopher.java
+++ src/main/java/com/pockocmoc/Philosopher.java
@@ -1,2 +1,54 @@
-package com.pockocmoc;public class Philosopher {
+package com.pockocmoc;
+
+
+import static com.pockocmoc.DiningPhilosophers.NUM_EATING_ITERATIONS;
+
+public class Philosopher extends Thread {
+    private final int id;
+    private final Object leftFork;
+    private final Object rightFork;
+
+    public Philosopher(int id, Object leftFork, Object rightFork) {
+        this.id = id;
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < NUM_EATING_ITERATIONS; i++) {
+            think();
+            eat();
+        }
+    }
+
+    private void think() {
+        System.out.println("Философ " + id + " размышляет");
+        try {
+            Thread.sleep((long) (Math.random() * 1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void eat() {
+        synchronized (leftFork) {
+            System.out.println("Философ " + id + " взял левую вилку");
+
+            synchronized (rightFork) {
+                System.out.println("Философ " + id + " взял правую вилку");
+
+                System.out.println("Философ " + id + " ест");
+                try {
+                    Thread.sleep((long) (Math.random() * 1000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("Философ " + id + " освободил правую вилку");
+            }
+
+            System.out.println("Философ " + id + " освободил левую вилку");
+        }
+    }
 }
